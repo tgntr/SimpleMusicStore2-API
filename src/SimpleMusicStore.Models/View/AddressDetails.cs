d97772ce31f01d381e6b29cc3a064535b379ff14
tgntr <tgntrr@gmail.com>
2019-06-05 12:19:30 +0300

    Rename web models for consistency

---
+++ src/SimpleMusicStore.Models/View/AddressDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleMusicStore.Models.View
+{
+    public class AddressDetails
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ src/SimpleMusicStore.Models/View/ArtistDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleMusicStore.Models.View
+{
+    public class ArtistDetails
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
